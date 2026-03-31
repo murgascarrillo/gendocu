42184617b5671cf9747f1945ceabf3aada887600
murgascarrillo <conclave@tutanota.com>
2026-03-31 14:07:04 -0500

    vis

---
+++ scripts/actions.js
@@ -0,0 +1,7 @@
+// ¿Cómo enviar el archivo pdf hacia Whatsapp?
+// Para enviar un archivo PDF a través de WhatsApp, puedes seguir estos pasos:
+// 1. Genera el PDF: Asegúrate de tener el PDF generado y guardado en tu dispositivo.
+// 2. Abre WhatsApp: Inicia la aplicación de WhatsApp en tu dispositivo móvil o en tu computadora.
+// 3. Selecciona el contacto o grupo: Elige el contacto o grupo al que deseas enviar el PDF.
+// 4. Adjunta el archivo: En la conversación, busca el ícono de adjuntar (generalmente representado por un clip) y selecciona "Documento" o "Archivo". Luego, navega hasta la ubicación donde guardaste el PDF y selecciónalo.
+// 5. Envía el mensaje: Después de adjuntar el PDF, puedes agregar un mensaje si lo deseas y luego presiona el botón de enviar para compartir el archivo con tu contacto o grupo en WhatsApp.
